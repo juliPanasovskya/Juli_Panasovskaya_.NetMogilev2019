b865bc5e309b0f962904aa31d321e7603acdc60d
Julia-sp <juli.panasovskya@gmail.com>
2019-10-10 22:00:11 +0300

    New project was created.

---
+++ Module1_1/Module1_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c;
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("a={0}, b={1}",a.ToString(), b.ToString());
+            Console.ReadLine();
+        }
+    }
+}
