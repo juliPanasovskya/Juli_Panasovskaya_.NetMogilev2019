ae5feee49948e3e9c16f96d266964250c5a1011a
Julia-sp <juli.panasovskya@gmail.com>
2019-10-17 21:55:32 +0300

    Task 2 is completed.

---
+++ Module_2/Module_2_3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Char separator = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.CurrencyDecimalSeparator[0];
+        Repeat:
+            Console.Clear();
+            Console.Write("Enter number A: ");
+            string numberA = Console.ReadLine().Replace('.', separator);
+            Console.Write("Enter number B: ");
+            string numberB = Console.ReadLine().Replace('.', separator);
+            double A;
+            double B;
+            double temp;
+            if (double.TryParse(numberA, out A))
+            {
+               
+
+                if (double.TryParse(numberB, out B))
+                {
+                    temp = A;
+                    A = B;
+                    B = temp;
+
+                    Console.WriteLine("A = {0} \nB = {1} ", A, B);
+
+                }
+                else
+                {
+                    Console.WriteLine("The value of number B isn't accepted. You have entered the letters.Try again!");
+                    Console.ReadKey();
+                    goto Repeat;
+                }
+            }
+            else
+            {
+                Console.WriteLine("The value of number A isn't accepted. You have entered the letters.Try again!");
+                Console.ReadKey();
+                goto Repeat;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+    }
+
---
+++ Module_2/Module_2_4/Round.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_2_4
+{
+  public  class Round
+    {
+        public double CalculatePerimetrRound(double radius)
+        {
+            double perimetrRound = Math.Round(2 * Math.PI * radius, 2);
+            Console.WriteLine("Perimetr of the round = {0}\n ", perimetrRound);
+            return perimetrRound;
+        }
+        public void CalculateRadiusRoundByPerimetr(double perimetr)
+        {
+            double radius = Math.Round(perimetr / (Math.PI * 2), 2);
+            Console.WriteLine("Radius of the round, R:{0}\n", radius);
+        }
+        public double CalculateSquareRound(double radius)
+        {
+            double squareRound = Math.Round(Math.PI * radius * radius, 2);
+            Console.WriteLine("Square of the round = {0}\n", squareRound);
+            return squareRound;
+        }
+        public void CalculateRadiusRoundBySquare(double square)
+        {
+            double radius = Math.Round(Math.Sqrt(square / (Math.PI)), 2);
+            Console.WriteLine("Radius of the round, R:{0}\n", radius);
+        }
+
+    }
+}
