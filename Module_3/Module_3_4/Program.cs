2122d00bb18fd0d6d4d326c497c180a369a55f5e
Julia-sp <juli.panasovskya@gmail.com>
2019-10-24 22:40:01 +0300

    Tasks 3.1-3.7

---
+++ Module_3/Module_3_4/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            string stringNumbers = Console.ReadLine();
+
+            char[] array = stringNumbers.ToCharArray();
+
+            int number = Convert(OverNumbers(array));
+
+            Console.WriteLine($"{number}");
+
+            Console.ReadKey();
+        }
+
+        static char[] OverNumbers(char[] array)
+        {
+            char temp;
+            int j = array.Length - 1;
+
+            for (int i = 0; i < j; i++)
+            {
+
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                j--;
+            }
+
+
+            return array;
+        }
+        static int Convert(char[] array)
+        {
+            string str1 = null;
+            string str = null;
+            foreach (char item in array)
+            {
+                str1 = item.ToString();
+                str = string.Concat(str, str1);
+            }
+
+
+            int number = int.Parse(str);
+            return number;
+        }
+
+
+
+    }
+}
