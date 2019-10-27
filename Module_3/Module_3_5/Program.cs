a74818d6385950118649f5e1a96016f641ff7113
Julia-sp <juli.panasovskya@gmail.com>
2019-10-27 21:08:06 +0300

    New project was created. Task 3 was complited

---
+++ Module_3/Module_3_5/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_3_5
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+            Console.Write("Enter numbers: ");
+
+                string stringNumbers = Console.ReadLine();
+
+            Console.Write("Enter number for deleting: ");
+
+                char charNumber = char.Parse(Console.ReadLine());
+
+                char[] array = stringNumbers.ToCharArray();
+
+                int number = Convert(DeleteNumber(array, charNumber));
+
+                Console.WriteLine($"{number}");
+
+                Console.ReadKey();
+            }
+        /// <summary>
+        /// Deleting selected digit from the number
+        /// </summary>
+        /// <param name="array">Array of char</param>
+        /// <param name="charNumber">Array of char </param>
+        /// <returns></returns>
+            static char[] DeleteNumber(char[] array, char charNumber)
+            {
+                char[] array2 = new char[array.Length - 1];
+                int k = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] == charNumber)
+                    {
+                        for (int j = i + 1; j < array.Length; j++)
+                        {
+                            array2[k] = array[j];
+                            k++;
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        array2[k] = array[i];
+                        k++;
+                    }
+
+                }
+
+
+                return array2;
+            }
+        /// <summary>
+        /// Converting array of char to the number
+        /// </summary>
+        /// <param name="array">Array of char</param>
+        /// <returns>number</returns>
+            static int Convert(char[] array)
+            {
+                string str1 = null;
+                string str = null;
+                foreach (char item in array)
+                {
+                    str1 = item.ToString();
+                    str = string.Concat(str, str1);
+                }
+
+
+                int number = int.Parse(str);
+                return number;
+            }
+        }
+    
+}
---
+++ Module_3/Module_3_7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_3_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 1, 0, 4, 7, 5 };
+            Console.Write("The base array:");
+            foreach (int item in array)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Console.WriteLine();
+
+            DisplayMaxElement(array);
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Display numbers wich previous element is smaller than the current one
+        /// </summary>
+        /// <param name="array">Smaller numbers</param>
+        static void DisplayMaxElement(int[] array)
+        {
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                int j = i + 1;
+                if (array[j]>array[i])
+                {
+                    Console.Write($" {array[j]} ");
+                }
+            }
+        }
+    }
+}
